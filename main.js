f9dab2597656b2973d454ff47c86681db88f6019
emborromeo <ericaborromeo06@gmail.com>
2020-10-22 23:51:24 +0800

    final w e

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 hpUrl=`http://hp-api.herokuapp.com/api/characters`;
 const searchBtn=()=>{
-    const typedName=document.getElementById('search-reads').value;
+    const typedName=document.getElementById("search-reads").value;
     
 fetch(hpUrl)
 .then(response=>response.json())
@@ -9,7 +9,7 @@ fetch(hpUrl)
     let hpObj = data.find(hp =>typedName.toLowerCase()===hp.name.toLowerCase() );
     console.log(hpObj.name);
 
-    document.querySelector('#image').src=hpObj.image;   
+    document.querySelector("#image").src=hpObj.image;   
     document.querySelector("#name").innerHTML=hpObj.name;
     document.querySelector("#bDay").innerHTML=hpObj.dateOfBirth;
     document.querySelector("#eyeColor").innerHTML=hpObj.eyeColour;
@@ -21,50 +21,32 @@ fetch(hpUrl)
 });
 };
 const viewAll=()=>{
-fetch(hpUrl)
-.then(response=>response.json())
-.then(data=>{  
-    console.log(data[1]); 
-    console.log(data.length);
-    //document.getElementById("view").style.display = "none";
 
-    for(i=0; i<=data.length-1;i++){
-    var newChar = document.getElementsByClassName('view-characters')[0];
-    var viewChar = newChar.cloneNode(true);
-    const viewAll=document.createElement('div');
-    document.body.appendChild(viewAll);
-    viewAll.appendChild(viewChar);
+    fetch(hpUrl)
+    .then(response=>response.json())
+    .then(data=>{  
+        console.log(data.length);
+        
+        for(i=0; i<=data.length;i++){ 
+           
+            var newChar = document.getElementsByClassName("view-characters")[0];
+            var viewChar = newChar.cloneNode(true);
+            const viewAll=document.createElement('div');
+            document.body.appendChild(viewAll);
+            viewAll.appendChild(viewChar);
+            console.log(data[i].image);
+            document.querySelector('#image').src=data[i].image;   
+            document.querySelector("#name").innerHTML=data[i].name;
+            document.querySelector("#bDay").innerHTML=data[i].dateOfBirth;
+            document.querySelector("#eyeColor").innerHTML=data[i].eyeColour;
+            document.querySelector("#hairColor").innerHTML=data[i].hairColour;
+            document.querySelector("#gender").innerHTML=data[i].gender;
+            document.querySelector("#house").innerHTML=data[i].house;
+            document.querySelector("#ancestry").innerHTML=data[i].ancestry;
+            document.querySelector("#patronus").innerHTML=data[i].patronus;
+         
 
-    document.querySelector('#image').src=data[i].image;   
-    document.querySelector("#name").innerHTML=data[i].name;
-    document.querySelector("#bDay").innerHTML=data[i].dateOfBirth;
-    document.querySelector("#eyeColor").innerHTML=data[i].eyeColour;
-    document.querySelector("#hairColor").innerHTML=data[i].hairColour;
-    document.querySelector("#gender").innerHTML=data[i].gender;
-    document.querySelector("#house").innerHTML=data[i].house;
-    document.querySelector("#ancestry").innerHTML=data[i].ancestry;
-    document.querySelector("#patronus").innerHTML=data[i].patronus;
-<<<<<<< HEAD
     }
-=======
-    //document.body.appendChild(viewChar);
-   
- }    
-
-    /*
-    const newDiv=document.createElement('div');
-    newDiv.className='view-characters';
-    const imgDiv=document.createElement('img');
-    imgDiv.className='image';
-    const newInfo=document.createElement('div');
-    newInfo.className('hp-info');
-    document.body.appendChild(newDiv);
-    newDiv.appendChild(imgDiv);
-    newDiv.appendChild(newInfo);
-
-<<<<<<< HEAD
-*/
+            viewAll[0].style.display="none";
 });   
 }
-
->>>>>>> e571714aecdc9035a4def54cbd7e89fcf27740ef
