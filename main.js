4df6ac47c56894c5b86fa1b4236e491dfffac38e
emborromeo <ericaborromeo06@gmail.com>
2020-10-24 12:32:41 +0800

    char search

--- main.js
+++ main.js
@@ -1,50 +1,52 @@
 const hpUrl=`http://hp-api.herokuapp.com/api/characters`;
-const searchBtn=()=>{
-    const typedName=document.getElementById("search-reads").value;
-    
-fetch(hpUrl)
-.then(response=>response.json())
-.then(data=>{
-    
-    let hpObj = data.find(hp =>typedName.toLowerCase()===hp.name.toLowerCase() );
-    console.log(hpObj.name);
 
-    document.querySelector("#image").src=hpObj.image;   
-    document.querySelector("#name").innerHTML=hpObj.name;
-    document.querySelector("#bDay").innerHTML=hpObj.dateOfBirth;
-    document.querySelector("#eyeColor").innerHTML=hpObj.eyeColour;
-    document.querySelector("#hairColor").innerHTML=hpObj.hairColour;
-    document.querySelector("#gender").innerHTML=hpObj.gender;
-    document.querySelector("#house").innerHTML=hpObj.house;
-    document.querySelector("#ancestry").innserHTML=hpObj.ancestry;
-    document.querySelector("#patronus").innerHTML=hpObj.patronus;
-});
+const searchBtn = () => {
+
+    const typedName = document.getElementById("search-reads").value;
+    
+    fetch(hpUrl)
+    .then(response => response.json())
+    .then(data => {
+        
+        let hpObj = data.find(hp =>typedName.toLowerCase() === hp.name.toLowerCase());
+        console.log(hpObj.name);
+
+        document.querySelector("#image").src = hpObj.image;   
+        document.querySelector("#name").innerHTML = hpObj.name;
+        document.querySelector("#bDay").innerHTML = hpObj.dateOfBirth;
+        document.querySelector("#eyeColor").innerHTML = hpObj.eyeColour;
+        document.querySelector("#hairColor").innerHTML = hpObj.hairColour;
+        document.querySelector("#gender").innerHTML = hpObj.gender;
+        document.querySelector("#house").innerHTML = hpObj.house;
+        document.querySelector("#ancestry").innserHTML = hpObj.ancestry;
+        document.querySelector("#patronus").innerHTML = hpObj.patronus;
+    });
 };
-const viewAll=()=>{
+const viewAll = () => {
 
     fetch(hpUrl)
-    .then(response=>response.json())
-    .then(data=>{  
+    .then(response => response.json())
+    .then(data => {  
         console.log(data.length);
         
-        document.getElementById("char-list").style.display="block";
+        document.getElementById("char-list").style.display = "block";
         let newChar = document.getElementsByClassName("view-characters")[0];
            
-         for(let i=0; i<=data.length;i++){ 
+         for(let i = 0; i <= data.length ; i++){ 
                 
             let viewChar = newChar.cloneNode(true);     
             document.body.appendChild(viewChar);
             console.log(data[i].image);
-            document.querySelector('#image').src=data[i].image;   
-            document.querySelector("#name").innerHTML=data[i].name;
-            document.querySelector("#bDay").innerHTML=data[i].dateOfBirth;
-            document.querySelector("#eyeColor").innerHTML=data[i].eyeColour;
-            document.querySelector("#hairColor").innerHTML=data[i].hairColour;
-            document.querySelector("#gender").innerHTML=data[i].gender;
-            document.querySelector("#house").innerHTML=data[i].house;
-            document.querySelector("#ancestry").innerHTML=data[i].ancestry;
-            document.querySelector("#patronus").innerHTML=data[i].patronus;
+            document.querySelector('#image').src = data[i].image;   
+            document.querySelector("#name").innerHTML = data[i].name;
+            document.querySelector("#bDay").innerHTML = data[i].dateOfBirth;
+            document.querySelector("#eyeColor").innerHTML = data[i].eyeColour;
+            document.querySelector("#hairColor").innerHTML = data[i].hairColour;
+            document.querySelector("#gender").innerHTML = data[i].gender;
+            document.querySelector("#house").innerHTML = data[i].house;
+            document.querySelector("#ancestry").innerHTML = data[i].ancestry;
+            document.querySelector("#patronus").innerHTML = data[i].patronus;
 
-   }
-});   
+         }
+    });   
 };
